2d397a1fc72833fd06588aefeaa2d574b5a49adf
m-colwell <mcolwell@gmail.com>
2018-10-03 09:47:19 -0400

    Added Timezone test

--- MacGitSandbox/src/mac/git/sandbox/Main.java
+++ MacGitSandbox/src/mac/git/sandbox/Main.java
@@ -1,9 +1,23 @@
 package mac.git.sandbox;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Git Sandbox.");
+        Date date = new Date();  
+
+        DateFormat formatter = new SimpleDateFormat("dd MMM yyyy HH:mm:ss z");
+
+        // Set the formatter to use a different timezone  
+        formatter.setTimeZone(TimeZone.getTimeZone("EST"));  
+
+        // Prints the date in the EST timezone  
+        System.out.println(formatter.format(date) + " == " + date);  
     }
 
 }
